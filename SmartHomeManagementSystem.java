e2a7efe0d13e616eeea79461051ff1f63131b984
Marat <139675556+Muratich@users.noreply.github.com>
2024-11-21 00:23:58 +0300

    Update SmartHomeManagementSystem.java

--- SmartHomeManagementSystem.java
+++ SmartHomeManagementSystem.java
@@ -1,5 +1,24 @@
-class SmartHomeManagementSystem {
+public class SmartHomeManagementSystem {
+    /**
+     * This function handles parsing commands
+     * from the input and <b>passing</b> each
+     * command to the
+     * <a href="#{@link}">{@link CommandHandler}</a> and <a href="#{@link}">{@link CommandExecutor}</a>.
+     */
     public static void main(String[] args) {
-        // https://youtu.be/xvFZjo5PgG0?si=oQ5bB91XOEyjwhk-
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> commands = new ArrayList<>();
+        while (true) {
+            String command = scanner.nextLine();
+            if (Objects.equals(command, "end")) {
+                break;
+            }
+            commands.add(command);
+        }
+        for (String command: commands) {
+            CommandHandler.check(command);
+        }
     }
 }
+// ВЕСЬ ОСТАВШИЙСЯ КОД ЗДЕСЬ ДЛЯ ТОГО ЧТОБЫ БРАУЗЕР НЕ ПАРСИЛ КОД
+//https://boulderbugle.com/github-u2tCNHED
